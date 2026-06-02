36600c7616a3814cc326ade91063e36030806bfb
IntentionedReflex35 <adamsisrael012@gmail.com>
2026-06-02 21:26:02 +0000

    refactor(library system): implement changes to borrow and get member info methods

--- Library_System.py
+++ Library_System.py
@@ -28,46 +28,49 @@ class Members(ABC):
         self.member_id = member_id
         self.borrowed_books = []
 
-    def add_books(self, book):
-        self.borrowed_books.append(book)
+#    def add_books(self, book):
+#        return self.borrowed_books.append(book)
 
     @abstractmethod
-    def get_member_info(self):
-        pass
-
-    @abstractmethod
-    def borrow(self):
-        pass
+    def borrow(self, book):
+        print(self.borrowed_books.append(book))
 
     @abstractmethod
     def return_(self):
         pass
 
+    @abstractmethod
+    def get_member_info(self):
+        number_of_books = len(self.borrowed_books)
+        return number_of_books
+
 
 class RegularMember(Members):
-    def get_member_info(self):
-        print(f"Name: {self.name}\t  Member_id: {self.member_id}")
-
-    def borrow(self):  # Can only borrow a maximum of 3 books
+    def borrow(self, book):  # Can only borrow a maximum of 3 books
         max_books = 3
         if len(self.borrowed_books) > max_books:
-            print(f"Maximum number of books({max_books}) reached. upgrade to premium if you want to borrow more.")
+            print(f"Maximum number of books({max_books}) reached. Upgrade to premium to borrow more.")
         else:
             print("Allowed")
 
     def return_(self):
         pass
 
+    def get_member_info(self):
+        print(f"Name: {self.name}\nMember_id: {self.member_id}\nNumber of borrowed books: "
+              f"{super().get_member_info()}")
+
 
 class PremiumMember(Members):
-    def get_member_info(self):
-        print(f"Name: {self.name} \n Member_id: {self.member_id}")
-
-    def borrow(self):    # Can only borrow a maximum of ten books
+    def borrow(self, book):    # Can only borrow a maximum of ten books
         pass
 
     def return_(self):
         pass
+
+    def get_member_info(self):
+        print(f"Name: {self.name}\nMember_id: {self.member_id}\nNumber of borrowed books: "
+              f"{super().get_member_info()}")
 
 
 class Library:
@@ -78,9 +81,8 @@ book0 = Books('garde', "john favour", "st235", True)
 book0.borrow_book()
 
 member1 = RegularMember("Ivan", "st.235")
+member1.borrow("Legend Of The Seeker")
+member1.borrow("Marvel")
+member1.borrow("America")
+member1.borrow("War")
 member1.get_member_info()
-member1.add_books("Legend Of The Seeker")
-member1.add_books("Marvel")
-member1.add_books("America")
-member1.add_books("War")
-member1.borrow()
