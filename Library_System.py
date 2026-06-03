c2d23e67c5829b84d6a7c71fca392fd9f59bd697
IntentionedReflex35 <adamsisrael012@gmail.com>
2026-06-03 17:15:24 +0000

    refactor(library system): implement changes regular and premium classes add assert to these classes

--- Library_System.py
+++ Library_System.py
@@ -28,49 +28,56 @@ class Members(ABC):
         self.member_id = member_id
         self.borrowed_books = []
 
-#    def add_books(self, book):
-#        return self.borrowed_books.append(book)
+    @abstractmethod
+    def borrow(self, book):       # Books borrowed will be added to the list
+        self.borrowed_books.append(book)
 
     @abstractmethod
-    def borrow(self, book):
-        print(self.borrowed_books.append(book))
-
-    @abstractmethod
-    def return_(self):
-        pass
+    def return_(self, book):       # Books returned will be removed from the list
+        self.borrowed_books.remove(book)
+        print(f"This book {book} has been returned.")
 
     @abstractmethod
     def get_member_info(self):
         number_of_books = len(self.borrowed_books)
-        return number_of_books
+        member_status = ['Regular', 'Premium']
+        return number_of_books, member_status
 
 
 class RegularMember(Members):
     def borrow(self, book):  # Can only borrow a maximum of 3 books
         max_books = 3
+        super().borrow(book)
         if len(self.borrowed_books) > max_books:
             print(f"Maximum number of books({max_books}) reached. Upgrade to premium to borrow more.")
-        else:
-            print("Allowed")
+            print(self.borrowed_books[:-1])
+        assert len(self.borrowed_books) <= max_books, f"Books borrowed should at most be {max_books}"
 
-    def return_(self):
-        pass
+    def return_(self, book):
+        super().return_(book)
 
     def get_member_info(self):
         print(f"Name: {self.name}\nMember_id: {self.member_id}\nNumber of borrowed books: "
-              f"{super().get_member_info()}")
+              f"{super().get_member_info()[0]}\n"
+              f"Member Status: {super().get_member_info()[1][0]}")
 
 
 class PremiumMember(Members):
     def borrow(self, book):    # Can only borrow a maximum of ten books
-        pass
+        max_books = 10
+        super().borrow(book)
+        if len(self.borrowed_books) > max_books:
+            print(f"Maximum number of books({max_books}) reached. Upgrade to premium to borrow more.")
+            print(self.borrowed_books[:-1])
+        assert len(self.borrowed_books) <= max_books, f"Books borrowed should at most be {max_books}"
 
-    def return_(self):
+    def return_(self, book):
         pass
 
     def get_member_info(self):
         print(f"Name: {self.name}\nMember_id: {self.member_id}\nNumber of borrowed books: "
-              f"{super().get_member_info()}")
+              f"{super().get_member_info()[0]}\n"
+              f"Member Status: {super().get_member_info()[1][1]}")
 
 
 class Library:
@@ -84,5 +91,12 @@ member1 = RegularMember("Ivan", "st.235")
 member1.borrow("Legend Of The Seeker")
 member1.borrow("Marvel")
 member1.borrow("America")
-member1.borrow("War")
+member1.return_("Legend Of The Seeker")
+# member1.borrow("War")
 member1.get_member_info()
+print('=====================================================')
+member2 = PremiumMember("Arthur", "st.236")
+member2.borrow("little")
+member2.borrow('sun')
+member2.borrow('hey there')
+member2.get_member_info()
