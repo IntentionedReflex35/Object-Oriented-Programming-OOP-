46194f2cf5bd237b9bb14394cdac4c9beb59bc3f
IntentionedReflex35 <adamsisrael012@gmail.com>
2026-06-18 07:44:15 +0000

    feat: implement show_question function to store display questions codes

--- quiz/oop_quiz.py
+++ quiz/oop_quiz.py
@@ -189,11 +189,15 @@ def show_welcome():
 
 show_welcome()
 
-# show questions
-divider()
-index = 1
-total = 10
-print(f"  {BOLD}Question {index} of {total}{RESET}")
-print()
-slow_print(f"  {BOLD}{questions[0]['question']}{RESET}", delay=0.02)
 
+# show questions
+def show_question():
+    divider()
+    index = 1
+    total = 10
+    print(f"  {BOLD}Question {index} of {total}{RESET}")
+    print()
+    slow_print(f"  {BOLD}{questions[0]['question']}{RESET}", delay=0.02)
+
+
+show_question()
